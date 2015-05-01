292bf29efc749bf986f9916c69e29bfc2a7cacbf
Techcable <Techcable@outlook.com>
2015-05-01 14:49:18 +0000

    Open Source it

--- src/main/java/net/techcable/spawnshield/tasks/KnockbackTask.java
+++ src/main/java/net/techcable/spawnshield/tasks/KnockbackTask.java
@@ -1,26 +1,24 @@
 /**
- * Copyright (c) 2015 Nicholas Schlabach
+ * The MIT License
+ * Copyright (c) 2014-2015 Techcable
  *
- * If there is conflict between two versions of this license, [the version found on gtihub](https://gist.github.com/Techcable/ea146b481870e3736b48) will take precedence.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * Permission is granted to operate this program provided that:
- * - You have purchased this plugin from Nicholas Schlabach (Techcable)
- * - Techcable has given you his express permission to operate/run this program
- * - Techcable has released this software publicly on either dev.bukkit.org, spigotmc.org or another software hosting site
- * - You do not modify this software in any way
- * - You do not redistrubute this software to anyone else
- * - You do not attempt to deobfuscate, decompile, or reverese engneer this plugin in any way
- * - Nicholas Schlabach (Techcable) reserves the right to change these terms an conditions at any time, with or without warning
- *
- * If any of the above terms are violated this plugin is no longer valid and you must stop using this software.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 package net.techcable.spawnshield.tasks;
 
